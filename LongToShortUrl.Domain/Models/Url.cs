e4aa5a35f86f605f6b8c032df6b70db80a0fb6e8
Ivn <i27897033@gmal.com>
2022-09-19 23:42:05 +0700

    Refactoring

---
+++ LongToShortUrl.Domain/Models/Url.cs
@@ -0,0 +1,11 @@
+﻿namespace LongToShortUrl.Domain
+{
+    public class Url
+    {
+        public int Id { get; set; }
+        public string LongUrl  { get; set; }
+        public string ShortUrl { get; set; }
+        public string UrlCode { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ LongToShortUrl.Domain/Services/IUrlShorteningService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongToShortUrl.Domain.Services
+{
+    public interface IUrlShorteningService
+    {
+        Url CreateShortUrl (string longUrl,int seed);
+    }
+}
